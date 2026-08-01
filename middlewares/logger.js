3f3623e3bf61035b9ea21fe53093c6c590bf172e
Mouaz Albitar <mouazalbitar@gmail.com>
2026-08-01 17:44:42 +0300

    add logger

---
+++ middlewares/logger.js
@@ -0,0 +1,12 @@
+function logger(req, res, next) {
+    // res.setHeader("Access-Control-Allow-Origin", "*"); // الوظيفة: يسمح بالوصول إلى الخادم من أي مصدر (Origin).
+    // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    // res.setHeader(
+    //     "Access-Control-Allow-Headers",
+    //     "Content-Type, Authorization",
+    // );
+    console.log(`${req.protocol}://${req.get("Host")}, ${req.method} ${req.originalUrl}`);
+    next();
+};
+
+module.exports = logger;
