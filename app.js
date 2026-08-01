592049f4444d5040eeeb29905cb06ab2efbbf18a
Mouaz Albitar <mouazalbitar@gmail.com>
2026-08-01 18:07:16 +0300

    add welcome page

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const {not_found, error_handler} = require("./middlewares/errors");
+const { not_found, error_handler } = require("./middlewares/errors");
+const logger = require("./middlewares/logger");
 const path = require("path"); // for welcome page
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
@@ -14,17 +15,17 @@ try {
     console.error("failed to connect to database,", err);
 }
 
-
 // apply middlewares
 app.use(express.json());
+app.use(logger);
 
-// <-- (2) السماح بقراءة الملفات الثابتة مثل الصور الموجودة في نفس المجلد
-app.use(express.static(__dirname));
+// السماح بقراءة الملفات الثابتة مثل الصور الموجودة في المجلد محدد
+app.use(express.static("welcome_page"));
 
 // routes
 app.use("/api/upload", require("./routes/upload"));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "welcome.html"));
+    res.sendFile(path.join(__dirname, "./welcome_page/welcome.html"));
 });
 app.use(not_found);
 app.use(error_handler);
