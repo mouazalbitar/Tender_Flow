33075310cb69355d3655d64eedd73c1b5ab85fc5
Mouaz Albitar <mouazalbitar@gmail.com>
2026-08-12 21:11:07 +0300

    add photo to user

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -10,13 +10,24 @@ const storage = multer.diskStorage({
             new Date().toISOString().replace(/:/g, "-") +
             "-" +
             Math.round(Math.random() * 1e9);
-        cb(
-            null,
-            `${uniqueSuffix}${path.extname(file.originalname)}`,
-        );
+        cb(null, `${uniqueSuffix}${path.extname(file.originalname)}`);
     },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5 MB
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+
+        if (!allowedTypes.includes(file.mimetype)) {
+            return cb(new Error("Only JPEG, JPG, and PNG images are allowed."));
+        }
+
+        cb(null, true);
+    },
+});
 
 module.exports = upload;
